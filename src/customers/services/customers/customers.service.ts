2bc50eaa916b41f5568b2d154fc5bf1e5ad5e8bd
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-02 23:47:53 +0700

    update user data

--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CustomerEditModel } from 'src/customers/controllers/models/customer.Edit';
+import { CustomerDtoModel } from 'src/customers/controllers/models/customer.dto';
 
 @Injectable()
 export class CustomersService {
-private users = [
+private users: Array<CustomerDtoModel> = [
     {
         id: 1,
         name: "xpnguyen",
+        email: "xpnguyen@gmail.com",
         createAt: new Date(),
     },
     {
         id: 2,
         name: "quyen",
+        email: "2115260@dlu.edu.vn",
         createAt: new Date(),
     }
 ]
