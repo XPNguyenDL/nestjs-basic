3326598dc26a9e73c62cfe8ac974ce0f1ce8395d
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-02 23:47:41 +0700

    add customer validator

--- src/customers/controllers/models/customer.Edit.ts
+++ src/customers/controllers/models/customer.Edit.ts
@@ -1,5 +1,20 @@
+import {
+    IsNotEmpty,
+    IsNumberString,
+    IsEmail
+} from "class-validator";
+
 export class CustomerEditModel {
+
+    @IsNotEmpty({ message: 'Id không được để trống'})
+    @IsNumberString({}, { message: 'Id không đúng định dạng'})
     id: number;
+
+    @IsNotEmpty({ message: 'Tên không được để trống'})
     name: string;
+
+    @IsEmail({}, { message: 'Không đúng định dạng email'})
+    email: string;
+
     createAt: Date = new Date();
 }
