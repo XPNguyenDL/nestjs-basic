34b8c079d3798bdb54a602b8de862b21a169dee1
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-03 16:15:06 +0700

    config mongo & module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+// Import controllers
+import { UsersController } from './controllers';
+
+// Import services
+import {
+  UsersService
+} from './services';
+import { User, UserSchema } from './core/entities';
+
 @Module({
-  imports: [    
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([{
+      name: User.name,
+      schema: UserSchema
+    }]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
   ],
-  controllers: [],
-  providers: [],
+  controllers: [
+    UsersController
+  ],
+  providers: [
+    UsersService
+  ],
 })
 export class AppModule { }
