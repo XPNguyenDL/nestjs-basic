390e874fab0677d49504d3742e4f4b2d2d1ef498
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-03 11:30:13 +0700

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
-import { ProductModule } from './modules/product/product.module';
-import { UsersModule } from './users/users.module';
 @Module({
-  imports: [
-    UserModule,
-    ProductModule,
-    UsersModule
+  imports: [    
   ],
   controllers: [],
   providers: [],
