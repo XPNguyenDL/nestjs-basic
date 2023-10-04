905e5ab1dddb0d17cc61044ea2cf52502170f30b
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-04 21:08:38 +0700

    add api response

---
+++ src/models/apiResponse.ts
@@ -0,0 +1,55 @@
+import { HttpStatus } from "@nestjs/common";
+
+export default class ApiResponse {
+    private _Errors: Array<string> = [];
+
+    public IsSuccess: boolean = typeof this.Errors === 'undefined' ? false : this.Errors.length === 0;
+
+
+    public StatusCode: HttpStatus;
+
+    constructor(statusCode: HttpStatus = HttpStatus.OK, errors: string[] = [], isSuccess: boolean = false) {
+        this.StatusCode = statusCode;
+        this.Errors = errors;
+        this.IsSuccess = isSuccess;
+    }
+
+    protected ApiResponse() {
+        this.StatusCode = HttpStatus.OK;
+        this.Errors = new Array<string>();
+    }
+
+    static Success<T>(result: T, statusCode: HttpStatus = HttpStatus.OK): ApiResponseWithReult<T> {
+        return new ApiResponseWithReult<T>(statusCode, [], result, true);
+    }
+
+    static FailWithResult<T>(statusCode: HttpStatus, result: T, ...errorMessages: string[]): ApiResponseWithReult<T> {
+        return new ApiResponseWithReult<T>(statusCode, errorMessages, result, false);
+    }
+
+    static Fail(statusCode: HttpStatus, ...errorMessages: string[]): ApiResponse {
+        if (errorMessages?.length === 0) {
+            throw new Error('errorMessages must not be empty.');
+        }
+
+        return new ApiResponse(statusCode, errorMessages, false);
+    }
+
+    public get Errors(): Array<string> {
+        return this._Errors;
+    }
+    public set Errors(value: Array<string>) {
+        this._Errors = value;
+    }
+}
+
+export class ApiResponseWithReult<T> extends ApiResponse {
+    public readonly Result: T;
+
+    constructor(statusCode: HttpStatus = HttpStatus.OK, errors: string[] = [], result?: T, isSuccess: boolean = false) {
+        super(statusCode, errors);
+        this.Result = result;
+        this.IsSuccess = isSuccess;
+    }
+
+}
