1aa523326e747b471e20b2e89a139961ac713235
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-04 21:08:46 +0700

    add home page

---
+++ src/controllers/homepage.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('')
+export class HomepageController {
+
+    @Get()
+    getHomepage() {
+        return {
+            "title": "Nestjs"
+        };
+    }
+}
