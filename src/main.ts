9dad42f8f8ac8645e26fbe62191f80da4e6cb171
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-16 14:47:32 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.use((req, res, next) => {
+    req.header('Access-Control-Allow-Origin', '*');
+    req.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    req.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, X-Auth-Token');
+    
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, X-Auth-Token');
