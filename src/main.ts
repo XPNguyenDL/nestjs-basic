d477a86e893a0d44e7b4d41146738dbb5f0412c3
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-16 14:28:15 +0700

    fix cors header

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,19 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Tutor connect')
     .setDescription('Tutor connect API description')
