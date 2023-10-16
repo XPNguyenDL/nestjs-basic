01a8654c47ce7c82850aaad15083274078c9f218
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-10-16 14:54:59 +0700

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,19 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    next();
-  });
+  const corsOptions: CorsOptions = {
+    origin: '*', // You can set specific origins here, e.g., 'https://example.com'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true, // Allow cookies and credentials in requests
+  };
 
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: [
-      'http://localhost:8100'
-    ],
-  });
-
-
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('Tutor connect')
